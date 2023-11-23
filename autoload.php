af8619b5b3e39028236d186261cfbd10416410db
Quentin Henry <quentin.henry@lg2.com>
2023-11-23 13:38:00 -0500

    autoload: delete directory

--- autoload.php
+++ autoload.php
@@ -11,21 +11,12 @@ spl_autoload_register(function ($className): void
     // Convertit les barres obliques inverses en barres obliques dans le nom de classe
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $className);
 
-    // Recherche dans les répertoires d'autoloading (ajustez selon votre structure)
-    $directories = [
-        __DIR__ . '/App/Controllers/',
-        __DIR__ . '/App/Models/',
-        __DIR__ . '/App/Views/',
-    ];
+    // Construire le chemin vers le fichier de classe
+    $file = __DIR__ . '/' . $class . '.php';
 
-    // Parcours les répertoires
-    foreach ($directories as $directory) {
-        $file = __DIR__ . '/' . $class . '.php';
-
-        // Vérifie si le fichier de classe existe
-        if (file_exists($file)) {
-            require_once $file;
-            return;
-        }
+    // Vérifie si le fichier de classe existe
+    if (file_exists($file)) {
+        require_once $file;
+        return;
     }
 });
